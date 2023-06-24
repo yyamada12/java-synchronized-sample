f943ed7c84288406b03bcaac47a2143bee9f5c2a
yyamada12 <12yacropolisy@gmail.com>
2023-06-24 15:30:12 +0900

    add run thread code

--- src/main/java/com/example/synchronizedsample/SynchronizedSampleApplication.java
+++ src/main/java/com/example/synchronizedsample/SynchronizedSampleApplication.java
@@ -1,13 +1,32 @@
 package com.example.synchronizedsample;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SynchronizedSampleApplication {
+public class SynchronizedSampleApplication implements ApplicationRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SynchronizedSampleApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        for (int i = 0; i < 30; i++) {
+            var i_ = String.valueOf(i);
+            Thread thread = new Thread(() -> {
+                System.out.println(i_);
+            });
+            thread.start();
+        }
+
+        // Wait for all threads to finish
+        Thread.sleep(1000);
+
+        System.out.println("Main Thread Finish");
     }
 
 }
