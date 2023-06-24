eb5c6804e91238652661b91c9c46938f8cbe0731
yyamada12 <12yacropolisy@gmail.com>
2023-06-24 14:40:22 +0900

    initial commit

---
+++ src/main/java/com/example/synchronizedsample/SynchronizedSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.synchronizedsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchronizedSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynchronizedSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/synchronizedsample/SynchronizedSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.synchronizedsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchronizedSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
